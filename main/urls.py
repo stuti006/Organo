c47dfb8d62d18f58972cdd1fc5818347d7d1a193
stuti006 <stutishrivastava006@gmail.com>
2022-08-25 17:47:26 +0530

    added funcationality of user auth

---
+++ main/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+      path('signup', views.signup, name='signup'),
+    # path('activate/<uidb64>/<token>', views.activate, name='activate'),
+    path('signin', views.signin, name='signin'),
+    path('signout', views.signout, name='signout'),
+]
